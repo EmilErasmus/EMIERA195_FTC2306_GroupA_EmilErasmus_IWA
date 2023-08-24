72fa992673a2612fe56e8f0108bc8b57ea97352e
Emil Erasmus <lime.erasmus@gmail.com>
2023-08-24 22:22:10 +0200

    Removed unnecessary function.

---
+++ IWA14-function-types/challenge-1/script.js
@@ -0,0 +1,10 @@
+firstName = 'John';
+age = 35;
+hobby = 'Coding';
+
+const logTwice = (parameter) => {
+    console.log(parameter)
+    console.log(parameter)
+}
+
+logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
